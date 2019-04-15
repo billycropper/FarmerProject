2d9c1500588cb0201f69b307a79ac50555fb2085
T'Shay Brooks <tshayb1209@gmail.com>
2019-04-15 16:40:47 -0400

    tshay final push

--- farmproject/src/main/java/Crop.java
+++ farmproject/src/main/java/Crop.java
@@ -1,17 +1,20 @@
 public abstract class Crop implements IProduce{
-    private Crop crop;
 
     public Crop() {
     }
-    public Object yield(){
-        return crop;
+
+    public void yield() {
+
     }
+
     public boolean edible() {
         return false;
     }
+
     public boolean hasBeenHarvested() {
         return false;
     }
+
     public boolean hasBeenFertilized() {
         return false;
     }
